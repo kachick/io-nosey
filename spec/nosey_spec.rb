eb02e0ac24326e658918183a8d00a1548e2394fa
Kenichi Kamiya <kachick1@gmail.com>
2013-05-13 03:43:58 +0900

    Write specs for #choose

--- spec/nosey_spec.rb
+++ spec/nosey_spec.rb
@@ -177,4 +177,41 @@ describe IO::Nosey::NoseyParker do
 
   end
 
+
+
+  describe "#choose" do
+
+    before :each do
+      @prompt = 'Choose an index: '
+      @obj = Object.new # Don't use String for example.
+                        # That returns other instanse via Hash key.
+      @choices = {
+        5      => '5 is a Integer',
+        @obj   => '"obj" is a Object',
+        :FIVE  => ':FIVE is a Symbol'
+      }
+    end
+
+    context "when choose an index" do
+      it "returns the relative value if selected 1" do
+        np_input 1
+        ret = @np.choose @prompt, @choices
+        expect(ret).to equal(5)
+      end
+
+      it "returns the relative value if selected 2" do
+        np_input 2
+        ret = @np.choose @prompt, @choices
+        expect(ret).to equal(@obj)
+      end
+
+      it "returns the relative value if selected 3" do
+        np_input 3
+        ret = @np.choose @prompt, @choices
+        expect(ret).to equal(:FIVE)
+      end
+    end
+
+  end
+
 end
